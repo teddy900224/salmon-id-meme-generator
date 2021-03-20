e4ba24e0aadbcceb91d4dcd9e8d6872001082a83
teddy900224 <teddy900224@gmail.com>
2021-03-21 04:13:44 +0800

    removed gh-page stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import SalmonIdGenerator from './SalmonIdGenerator';
-import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
     return (
-      <BrowserRouter basename={window.location.pathname || ''}>
-        <Route exact path="/" component={SalmonIdGenerator} />    
-      </BrowserRouter>        
+        <div className="App">
+          <SalmonIdGenerator/>      
+        </div>
     );
 }
 
