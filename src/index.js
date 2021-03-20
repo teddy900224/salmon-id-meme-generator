5ea1262de170fc154c5da73e6532761223f1e0a6
teddy900224 <teddy900224@gmail.com>
2021-03-21 03:14:27 +0800

    added router fix

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename ="/salmon-id-meme-generator">
+    <Route exact path="/" component={App} />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
